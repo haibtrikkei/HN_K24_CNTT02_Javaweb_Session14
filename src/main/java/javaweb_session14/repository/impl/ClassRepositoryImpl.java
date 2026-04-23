211febdfd5991af13783ed8d377af41a375ffb98
haibtrikkei <buithanhhai9380@gmail.com>
2026-04-23 09:54:42 +0700

    Update code Session 14

--- src/main/java/javaweb_session14/repository/impl/ClassRepositoryImpl.java
+++ src/main/java/javaweb_session14/repository/impl/ClassRepositoryImpl.java
@@ -6,6 +6,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Repository
--- src/main/java/javaweb_session14/repository/impl/StudentRepositoryImpl.java
+++ src/main/java/javaweb_session14/repository/impl/StudentRepositoryImpl.java
@@ -4,8 +4,11 @@ import javaweb_session14.model.entity.Student;
 import javaweb_session14.repository.StudentRepository;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
 
 @Repository
 public class StudentRepositoryImpl implements StudentRepository {
@@ -15,8 +18,8 @@ public class StudentRepositoryImpl implements StudentRepository {
     @Override
     public boolean insertStudent(Student student) {
         Session session = sessionFactory.openSession();
-
         try{
+            session.doWork(conn -> conn.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ));
             session.beginTransaction();
             session.persist(student);
             session.getTransaction().commit();
